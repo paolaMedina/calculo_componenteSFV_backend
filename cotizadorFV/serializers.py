b5d9ba133834d1afd3231c226e3c1bc16e6dfdb2
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-07-01 00:04:37 -0500

    Fix duplicated name at generalfv, renamed generalfv to general fv form  (that exists in both, models and generalFv in models form module)

--- cotizadorFV/serializers.py
+++ cotizadorFV/serializers.py
@@ -1,18 +1,26 @@
 
 
 from rest_framework import serializers
-from models_form.generalFV import GeneralFV
+from models_form.generalFV import GeneralFVForm
 from models_excel.dps_ac import DpsAC
 from models_excel.dps_dc import DpsDC
 
 
 class GeneralFVSerializer(serializers.ModelSerializer):
     class Meta:
-        model = GeneralFV
+        model = GeneralFVForm
         fields = ('power_of_plant_fv', 'total_panels_fv', 'power_of_panel_fv', 'ambient_temperature', 'lowest_ambient_temperature_expected',
         'investment_type', 'service_type', 'service_voltage', 'instalation_place')
 
-
+class Example:
+    def __init__(self, a, b):
+        self.a = a
+        "attribute Example.a doc-string (1)"
+        self.b = b
+        "attribute Example.b doc-string (2)"
+class ExampleSerializer(serializers.Serializer):
+    a = serializers.CharField()
+    b = serializers.CharField()
 class DpsACSerializer(serializers.Serializer):
     
     descripcion= serializers.CharField(max_length=100)
@@ -26,7 +34,7 @@ class DpsACSerializer(serializers.Serializer):
     in_in= serializers.IntegerField()
     imax_por_polo= serializers.CharField(max_length=100)
     iimp_por_polo= serializers.CharField(max_length=100)
-    up=serializers.DecimalField()
+    up=serializers.DecimalField(max_digits=50, decimal_places=25)
     telesenal= serializers.CharField(max_length=100)
     precio= serializers.CharField(max_length=100)
     
@@ -73,9 +81,9 @@ class DpsDCSerializer(serializers.Serializer):
      forma_constructiva= serializers.CharField(max_length=100)
      no_polos= serializers.CharField(max_length=100)
      uc= serializers.IntegerField()
-     in_in= serializers.DecimalField()
+     in_in= serializers.DecimalField(max_digits=50, decimal_places=25)
      imax= serializers.IntegerField()
      precio= serializers.CharField(max_length=100)
-     
+     # no hace falta por que no se va a guardar en la bd
      def create(self, validated_data):
          return DpsDC.objects.create(**validated_data)
