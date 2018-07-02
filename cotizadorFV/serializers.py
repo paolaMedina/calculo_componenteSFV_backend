8c5484387350406f7e18270e1c6455f3f2c943c5
paola medina <angie.paola.medina@correounivalle.edu.co>
2018-07-02 19:15:48 +0000

    Allowed hosts now allow all

--- cotizadorFV/serializers.py
+++ cotizadorFV/serializers.py
@@ -1,9 +1,5 @@
-
-
 from rest_framework import serializers
 from models_form.generalFV import GeneralFVForm
-from models_excel.dps_ac import DpsAC
-from models_excel.dps_dc import DpsDC
 
 
 class GeneralFVSerializer(serializers.ModelSerializer):
@@ -39,38 +35,6 @@ class DpsACSerializer(serializers.Serializer):
     precio= serializers.CharField(max_length=100)
     
 
-    def create(self, validated_data):
-        """
-        Crea y devuelve una nueva instancia de 'DpsAC', dados los datos validados.
-        """
-        return DpsAC.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Actualiza y devuelva una instancia existente de 'DpsAC', dados los datos validados..
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        
-        instance.descripcion = validated_data.get('descripcion', instance.descripcion )
-        instance.marca = validated_data.get('marca', instance.marca )
-        instance.referencia = validated_data.get('referencia', instance.referencia)
-        instance.tipo = validated_data.get('tipo', instance.tipo)
-        instance.clase_prueba = validated_data.get('clase_prueba', instance.clase_prueba)
-        instance.forma_constructiva = validated_data.get('forma_constructiva', instance.forma_constructiva)
-        instance.no_polos = validated_data.get('no_polos', instance.no_polos)
-        instance.uc = validated_data.get('uc', instance.uc)
-        instance.in_in = validated_data.get('in_in', instance.in_in)
-        instance.imax_por_polo = validated_data.get('imax_por_polo', instance.imax_por_polo)
-        instance.iimp_por_polo = validated_data.get('iimp_por_polo', instance.iimp_por_polo)
-        instance.up = validated_data.get('up', instance.up)
-        instance.telesenal = validated_data.get('telesenal', instance.telesenal)
-        instance.precio = validated_data.get('precio', instance.precio)
-        instance.save()
-        return instance
         
 class DpsDCSerializer(serializers.Serializer):
      descripcion= serializers.CharField(max_length=100)
@@ -84,6 +48,3 @@ class DpsDCSerializer(serializers.Serializer):
      in_in= serializers.DecimalField(max_digits=50, decimal_places=25)
      imax= serializers.IntegerField()
      precio= serializers.CharField(max_length=100)
-     # no hace falta por que no se va a guardar en la bd
-     def create(self, validated_data):
-         return DpsDC.objects.create(**validated_data)
--- cotizadorFV/views.py
+++ cotizadorFV/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ExampleSerializer, Example
+
 # Create your views here.
 """
 calculate():
