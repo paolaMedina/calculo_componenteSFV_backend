af3e227732bbf22ec1f9a829038dbbbe7185a0d3
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-07-04 00:01:02 -0500

    Example urls updated, added uploadlibro1example

--- cotizadorFV/urls.py
+++ cotizadorFV/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('', views.ExampleView.as_view(), name='index'),
+    url('exampleserializers', views.ExampleView.as_view(), name='index'),
+    url('uploadlibro1example', views.upload_libro1_example, name='uploadlibroexample'),
 ]
