5082b45e978a98fd87062c767921ab44ce099077
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-07-03 22:33:58 -0500

    Post example for serializers

--- cotizadorFV/views.py
+++ cotizadorFV/views.py
@@ -3,17 +3,21 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ExampleSerializer, Example
 # Create your views here.
-"""
-calculate():
-    deserialize(cotization ):
-        pdf = lib.generatepdf(cotization)"""
 class ExampleView(APIView):
-    def get(self, request, format=None):
-        """
-        Return a list of all users.
-        """
-        example = Example('field_a', 'field_b')
-        exampleSerializer = ExampleSerializer(example)
-        example = Example(**exampleSerializer.data)
-        print(example.a)
-        return Response(exampleSerializer.data)
+	def post(self, request):
+		exampleSerializer = ExampleSerializer(data=request.data)
+		if (exampleSerializer.is_valid()):
+			example = Example(a=exampleSerializer.data['a'], b=exampleSerializer.data['b'])
+			print(example.a,example.b)
+			return Response(exampleSerializer.data)
+		else:
+			return Response('nel')
+	def get(self, request, format=None):
+		"""
+		Return a list of all users.
+		"""
+		example = Example('field_a', 'field_b')
+		exampleSerializer = ExampleSerializer(example)
+		example = Example(**exampleSerializer.data)
+		print(example)
+		return Response(exampleSerializer.data)
