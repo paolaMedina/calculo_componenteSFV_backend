cde303683ce65361f4edb270b5390b77ee4b013b
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-07-07 10:27:20 -0500

    Import csv function moved to lib.py at models_excel

--- cotizadorFV/views.py
+++ cotizadorFV/views.py
@@ -9,20 +9,6 @@ from .serializers import ExampleSerializer, Example, InteManualSerializer
 """
 # Create your views here.
 """
-
-def importcsv(modelo, archivo):
-    
-    if (modelo=='InterruptorManual'):
-        my_csv_list = InterruptorManual.import_data(data = open(archivo))
-        
-        for i in range(1, len(my_csv_list)):
-            line = my_csv_list[i]
-            inicial.interruptores_manuales.append(line)
-    else :
-        0
-    
-    for a in inicial.interruptores_manuales:
-          print a.descripcion
          
 
 class InterruptorManualSerializerView(APIView):
