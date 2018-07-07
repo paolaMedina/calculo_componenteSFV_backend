7406f0f440ffff3bc5198dc92a224aed9ee509cb
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-07-07 10:26:21 -0500

    Main info updated with setters and paneles solares attribute

--- cotizadorFV/main_info.py
+++ cotizadorFV/main_info.py
@@ -1,8 +1,17 @@
 class MainInfo():
-    interruptores_manuales=[]
-    def __init__(self):
-         self.datos = []
-    
+    def __init__(self, interruptores_manuales=[], paneles_solares=[]):
+        """[summary]
+        
+        Keyword Arguments:
+            interruptores_manuales {list of InterruptoresManuales} -- [description] (default: {[]})
+            paneles_solares {list of PanelesSolares} -- [description] (default: {[]})
+        """
+        self.interruptores_manuales = interruptores_manuales
+        self.paneles_solares = paneles_solares
+    def setInterruptoresManuales(self, interruptores_manuales):
+        self.interruptores_manuales = interruptores_manuales
+    def setPanelesSolares(self, paneles_solares):
+        self.paneles_solares = paneles_solares
 
 
 inicial=MainInfo()
