01b15870afd66673ad9eea21569c4f2795baf4db
paola medina <angie.paola.medina@correounivalle.edu.co>
2018-07-16 04:07:18 +0000

    subir csv's completo

--- cargaArchivos/forms.py
+++ cargaArchivos/forms.py
@@ -4,15 +4,22 @@ from .widgets import KrajeeFileInputWidget
 from django.core.exceptions import ValidationError
 
 class FormUpload(forms.Form):
- 
+    
+    archivos= (
+    ('dps_ac', 'DPS_AC'),
+    ('dps_dc', 'DPS.DC'),
+    ('fusible', 'Fusibles'),
+    ('inte_auto', 'Interrupturos Automaticos'),
+    ('inte_man',  'Interrupturos Manuales'),
+    ('inversor', 'Inversores'),
+    ('microInve', 'MicroInversores'),
+    ('panelSolar', 'Paneles Solares'),
+    )
     '''
     Form principal
     '''
     #file = forms.FileField(widget= KrajeeFileInputWidget )
+    tipo_archivo=forms.ChoiceField(choices=archivos,label='Tipo de archivo',widget=forms.Select(attrs={'class':'form-control', 'required':'required'}))
     file = forms.FileField( )
-    def handle_uploaded_file(self, f):
-        print f.name
-        with open('archivos/' + f.name, 'w') as destination:
-            for chunk in f.chunks():
-                destination.write(chunk)
-                print chunk
+    
+    
